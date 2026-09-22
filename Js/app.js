03f5e8ff72b49e6ceb1b355b4445e601e0dfd510
Wilbertgarcia77 <elizabetmar09@gmail.com>
2026-09-21 22:02:56 -0600

    Add Flores Amarillas music

--- Js/app.js
+++ Js/app.js
@@ -11,6 +11,12 @@ window.addEventListener('focus', () => {
 
 let h1 = document.getElementById("Titulo");
 let Boton1 = document.getElementById("B1");
+const musicaFlores = document.getElementById("musicaFlores");
+
+document.getElementById("BVer").addEventListener("click", function() {
+  musicaFlores.play().catch(() => {});
+});
+
 Boton1.addEventListener('click', function() {
     const ContenedorBotones = document.querySelector(".Con");
     document.querySelector(".Texto").style.display = "block";
